b85548bfad6cd7bd91860d4649ecea82061afa16
KeerthiB25 <keerthidas18@gmail.com>
2025-10-27 13:10:05 -0500

    Initial commit

---
+++ src/main/java/com/bkpro/dsce/dsceservice/DsceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bkpro.dsce.dsceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsceServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bkpro/dsce/dsceservice/DsceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bkpro.dsce.dsceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
